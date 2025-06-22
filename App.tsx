408a19917c2e12bd25fb7721c53601ccb624f7dd
Giacomelli <133823261+lfgiacomelli@users.noreply.github.com>
2025-06-22 03:39:51 +0000

    Pending changes exported from your codespace

--- App.tsx
+++ App.tsx
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Loading } from '@components/Loading';
+
+import { View, StatusBar } from 'react-native';
+
+import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto'
+
+import { GluestackUIProvider, Text, Center } from '@gluestack-ui/themed';
+
+import { config } from './config/gluestack-ui.config';
+
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <GluestackUIProvider config={config}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? (
+        <SignIn />
+      ) : (
+        <Loading />
+      )}
+    </GluestackUIProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/SignIn.tsx
@@ -0,0 +1,9 @@
+import { VStack, Image } from "@gluestack-ui/themed";
+
+import BackgroundImg from '@assets/background.png';
+
+export function SignIn(){
+    return <VStack flex={1} bg="$gray700" >
+        <Image source={BackgroundImg}
+    </VStack>
+}
